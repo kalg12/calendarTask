61d9e09135b27ff2c20cb1f70cc26edb23e26489
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-03-27 12:27:20 -0600

    Fetching data from API in component

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,8 @@
-const API = process.env.API_URL;
+const baseURL = process.env.API_URL;
+const taskURL = `${baseURL}/task`;
+
+export const getTasks = async () => {
+  const response = await fetch(taskURL);
+  const data = await response.json();
+  return data;
+};
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 
-const Calendar = () => {
+const fetchTaskCalendar = async () => {
+  const resp = await fetch(process.env.API_URL);
+  const data = await resp.json();
+  return data;
+};
+
+const Calendar = async () => {
+  const taskCalendar = await fetchTaskCalendar();
   return (
     <div className="flex flex-wrap">
       <div className="w-full lg:w-1/2 px-4">
         <h2 className="text-2xl font-bold mb-4">Próximos eventos</h2>
         <ul>
-          <li className="mb-2">
-            <span className="font-bold">Evento 1:</span>
-            <span className="ml-2">Fecha del evento</span>
-          </li>
-          <li className="mb-2">
-            <span className="font-bold">Evento 2:</span>
-            <span className="ml-2">Fecha del evento</span>
-          </li>
-          <li className="mb-2">
-            <span className="font-bold">Evento 3:</span>
-            <span className="ml-2">Fecha del evento</span>
-          </li>
-          {/* Añade aquí más eventos */}
+          {taskCalendar.map((task) => (
+            <li
+              key={task.id}
+              className="bg-white rounded-lg shadow-lg p-4 mb-4"
+            >
+              <h3 className="text-xl font-bold">{task.title}</h3>
+              <p className="text-gray-600">{task.description}</p>
+              <p className="text-gray-600">
+                {task.eventDay} {task.startTime} - {task.endTime}
+              </p>
+              <p className="text-gray-600">
+                Asistencia esperada: {task.expectedAssistance}
+              </p>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="w-full lg:w-1/2 px-4">
